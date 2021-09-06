0ad983cdc5ae3977eaba4835ea7c63c9dfe9d91d
JG HAN <gojkhan13@gmail.com>
2021-09-06 22:50:13 +0900

    0906 서블릿 예외처리 - 인터셉터

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -1,14 +1,26 @@
 package hello.exception;
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Bean
+
+    //필터와 다르게 dispatcher type을 가지고 호출여부를 결정할 수 없지만 대신에 excludePathPatterns를 활용할 수 있다.
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**"); //오류 페이지 경로
+    }
+
+  //  @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new
                 FilterRegistrationBean<>();
